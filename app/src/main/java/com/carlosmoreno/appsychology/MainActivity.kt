da508d8282a6891419c3550dead82c8264b3406d
Carlos Moreno <ing.karlos.17@gmail.com>
2022-10-25 00:00:34 -0500

    Se crea la vista principal

--- app/src/main/java/com/carlosmoreno/appsychology/MainActivity.kt
+++ app/src/main/java/com/carlosmoreno/appsychology/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.carlosmoreno.appsychology
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.carlosmoreno.appsychology.databinding.ActivityAboutBinding
+import com.carlosmoreno.appsychology.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSomos.setOnClickListener{
+            startActivity(Intent(this, ActivityAboutBinding::class.java) )
+        }
     }
 }
